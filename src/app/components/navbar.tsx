09a131a5ff5be8721579c107eba7d3e9e8ba5e22
Samuel Kosasih <samadrianko@gmail.com>
2025-03-10 23:50:32 -0400

    Better Navbar paddings for Mobile

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,7 +6,7 @@ import ThemeSwitcher from './theme-switcher';
 /** Navbar component. Handles dark mode toggle. */
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 z-50 flex justify-between items-center py-5 px-10">
+        <nav className="sticky top-0 z-50 flex justify-between items-center py-5 px-5 md:px-10">
             <div className="flex items-center justify-between">
                 <Link href="#" className="text-3xl font-semibold font-mono">
                     SAMUEL KOSASIH
