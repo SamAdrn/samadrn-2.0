227e7b595329e3dc9489edb33cd2ee895e479253
Samuel Kosasih <samadrianko@gmail.com>
2025-03-10 22:53:07 -0400

    Enhance Theme Toggle

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,38 +1,10 @@
 'use client';
 
-import { AnimatePresence, motion } from 'framer-motion';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import ThemeSwitcher from './theme-switcher';
 
 /** Navbar component. Handles dark mode toggle. */
 export default function Navbar() {
-    /** Keeps track of the dark mode preference */
-    const [darkMode, setDarkMode] = useState(true);
-
-    useEffect(() => {
-        const storedTheme = localStorage.getItem('theme');
-        const prefersDark = window.matchMedia(
-            '(prefers-color-scheme: dark)',
-        ).matches;
-
-        if (storedTheme === 'dark' || (!storedTheme && prefersDark)) {
-            setDarkMode(true);
-            document.documentElement.classList.add('dark');
-        }
-    }, []);
-
-    /** Toggles dark mode and saves the preference to localStorage */
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-        if (!darkMode) {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('theme', 'light');
-        }
-    };
-
     return (
         <nav className="sticky top-0 z-50 flex justify-between items-center py-5 px-10">
             <div className="flex items-center justify-between">
@@ -42,64 +14,7 @@ export default function Navbar() {
             </div>
 
             <div>
-                <button
-                    onClick={toggleDarkMode}
-                    className="cursor-pointer p-2 border-2 border-gray-600 rounded-full"
-                    aria-label={
-                        darkMode ? 'Toggle light mode' : 'Toggle dark mode'
-                    }
-                >
-                    {/* Width and height of the container div should match the width and height of the svg */}
-                    <div className="relative w-6 h-6">
-                        <AnimatePresence mode="wait" initial={false}>
-                            {darkMode ? (
-                                <motion.svg
-                                    key="sun"
-                                    initial={{ rotate: 90, opacity: 0 }}
-                                    animate={{ rotate: 0, opacity: 1 }}
-                                    exit={{ rotate: -90, opacity: 0 }}
-                                    transition={{
-                                        duration: 0.3,
-                                        ease: 'easeInOut',
-                                    }}
-                                    className="absolute inset-0 w-6 h-6 text-yellow-400"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                                    />
-                                </motion.svg>
-                            ) : (
-                                <motion.svg
-                                    key="moon"
-                                    initial={{ rotate: -90, opacity: 0 }}
-                                    animate={{ rotate: 0, opacity: 1 }}
-                                    exit={{ rotate: 90, opacity: 0 }}
-                                    transition={{
-                                        duration: 0.3,
-                                        ease: 'easeInOut',
-                                    }}
-                                    className="absolute inset-0 w-6 h-6 text-gray-700"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                                    />
-                                </motion.svg>
-                            )}
-                        </AnimatePresence>
-                    </div>
-                </button>
+                <ThemeSwitcher />
             </div>
         </nav>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+import Hero from './sections/hero';
+
+export default function Page() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1>Hello World</h1>
+        <div className="flex flex-col items-center justify-center">
+            <Hero />
         </div>
     );
 }
