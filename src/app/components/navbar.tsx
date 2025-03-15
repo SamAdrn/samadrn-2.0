24353e01d2509edf3f14880187a39f765c6190a5
Samuel Kosasih <samadrianko@gmail.com>
2025-03-15 01:03:54 -0400

    Nav hidden on < lg

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,9 +4,10 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import ThemeSwitcher from './theme-switcher';
 
-/** Navbar component. Handles dark mode toggle. */
+/** Navbar component. */
 export default function Navbar() {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isVisible, setIsVisible] = useState(true);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -14,21 +15,33 @@ export default function Navbar() {
             setIsScrolled(scrollPosition > 70);
         };
 
+        const handleResize = () => {
+            setIsVisible(window.innerWidth < 1024); // 64rem = 1024px
+        };
+
         // Add event listener when component mounts and run it once to set initial state
         window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
+
         handleScroll();
+        handleResize();
 
         // Clean up event listener when component unmounts
         return () => {
             window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
+
+    if (!isVisible) {
+        return null;
+    }
 
     return (
         <header>
             <nav
                 className={
-                    'fixed bg-transparent top-0 left-0 right-0 z-50 px-5 md:px-20 lg:px-[5%] flex justify-between items-center h-[var(--navbar-height)] transition-all duration-300 ' +
+                    'fixed bg-transparent top-0 left-0 right-0 z-50 px-5 md:px-20 flex justify-between items-center h-[var(--navbar-height)] transition-all duration-300 ' +
                     (isScrolled
                         ? 'backdrop-blur-sm bg-white/10 dark:bg-gray-900/10 shadow-xs'
                         : 'bg-transparent')
