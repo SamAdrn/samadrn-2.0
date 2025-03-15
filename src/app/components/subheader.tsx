62b6baf7f21a2750df55d5b7b4e4388999e51748
Samuel Kosasih <samadrianko@gmail.com>
2025-03-14 23:53:17 -0400

    Improve Subheader Look

--- src/app/components/subheader.tsx
+++ src/app/components/subheader.tsx
@@ -5,13 +5,16 @@ interface SubheaderProps {
 /** A bordered pill-shaped container for subheader text */
 export default function Subheader({ text }: SubheaderProps) {
     return (
-        <span
-            className={
-                'select-none font-bold text-2xl px-4 py-1 ' +
-                'underline decoration-[0.3rem] underline-offset-8 decoration-amber-600/50 dark:decoration-sky-600/50'
-            }
-        >
-            {text.toUpperCase()}
-        </span>
+        <div className="flex items-center">
+            <span className="hidden lg:inline h-[1px] w-6 ms-1 bg-surface-half-light/70 dark:bg-surface-half-dark"></span>
+            <span
+                className={
+                    'select-none font-bold text-xl ps-3 tracking-tight '
+                    // 'underline decoration-[0.3rem] underline-offset-8 decoration-amber-600/50 dark:decoration-sky-600/50'
+                }
+            >
+                {text.toUpperCase()}
+            </span>
+        </div>
     );
 }
