b20ad6418c198369de889b8bebb6d2006ec2d551
Samuel Kosasih <samadrianko@gmail.com>
2025-03-29 12:07:56 -0400

    Refine Visual Container

--- src/app/components/sections/visual/VisualContainer.tsx
+++ src/app/components/sections/visual/VisualContainer.tsx
@@ -3,9 +3,13 @@
 import CodeWindow from './CodeWindow';
 import { SectionContainer } from '../../shared';
 
+/** The Visual Section Component. Serves as the visual aspect of the page's entrance */
 export default function VisualContainer() {
     return (
-        <SectionContainer id='visual' styleClass='lg:!pt-0 lg:h-screen lg:justify-center'>
+        <SectionContainer
+            id="visual-section"
+            styleClass="lg:!pt-0 lg:h-screen lg:justify-center"
+        >
             <CodeWindow />
         </SectionContainer>
     );
--- src/app/components/shared/SectionContainer.tsx
+++ src/app/components/shared/SectionContainer.tsx
@@ -6,17 +6,18 @@ interface SectionContainerProps {
     styleClass?: string;
 }
 
+/** Creates a section within the page */
 export default function SectionContainer({
     children,
     id,
     styleClass = '',
 }: SectionContainerProps) {
     return (
-        <div
+        <section
             id={id}
             className={'w-full pt-24 lg:pt-35 flex flex-col ' + styleClass}
         >
             {children}
-        </div>
+        </section>
     );
 }
