263755ae75e4af6968a57886a598c53cbaaad9ed
Samuel Kosasih <samadrianko@gmail.com>
2025-04-04 00:38:48 -0400

    Refine Experience Container

--- src/app/components/sections/experience/ExperienceContainer.tsx
+++ src/app/components/sections/experience/ExperienceContainer.tsx
@@ -1,193 +1,144 @@
 'use client';
 
+import { experiences } from '@/app/lib/utils/Constants';
+import { useTranslation } from 'react-i18next';
 import { Chip, SectionContainer, Subheader } from '../../shared';
 
-interface ExperienceItem {
-    title: string;
-    place: string;
-    start: string;
-    end: string;
-    location: string;
-    points: string[];
-    categories: string[];
-    link: string;
-}
-
+/** The Experience Section Component.*/
 export default function ExperienceContainer() {
-    const items: ExperienceItem[] = [
-        {
-            title: 'Software Engineer L2',
-            place: 'Inovalon',
-            start: 'June 2024',
-            end: '',
-            location: 'Bowie, MD',
-            points: [
-                'Leveraged Angular to design and develop a wizard-style data-entry application to streamline customer onboarding processes from multiple weeks to a few hours',
-                'Developed a script to map metadata properties from Swagger documentation using schemas from various REST endpoints',
-                'Created schematics that output various types of Angular pages of an internal admin tool managing enterprise customer accounts',
-            ],
-            categories: [
-                'Angular',
-                'TypeScript',
-                'HTML + SCSS',
-                'PrimeNG',
-                'Python',
-            ],
-            link: 'https://www.inovalon.com/',
-        },
-        {
-            title: 'Web Developer Intern',
-            place: 'Indonesia Infrastructure Guarantee Fund (IIGF) Institute',
-            start: 'June 2023',
-            end: 'August 2023',
-            location: 'Jakarta, IDN (Remote)',
-            points: [
-                'Designed and developed an executive dashboard to facilitate infrastructure project investments for Indonesian stakeholders within a notable government institution',
-                'Collaborated with the Project Division to comprehend dashboard requirements, showcasing adaptability to project‑specific demands',
-            ],
-            categories: [
-                'Next.js',
-                'React',
-                'Tailwind',
-                'JavaScript',
-                'HTML + CSS',
-            ],
-            link: 'https://www.ptpii.co.id/',
-        },
-        {
-            title: 'Student Lead',
-            place: 'UMD Division of IT | Terrapin Tech',
-            start: 'November 2021',
-            end: 'May 2024',
-            location: 'College Park, MD',
-            points: [
-                'Assist members of the University of Maryland community in troubleshooting various software issues',
-                'Mentored team members on store procedures, prioritizing their well‑being and safety in a customer‑facing role',
-            ],
-            categories: ['Technical Support', 'Communication', 'Leadership'],
-            link: 'https://it.umd.edu/',
-        },
-        {
-            title: "Pursuing my Bachelor's",
-            place: 'University of Maryland, College Park',
-            start: 'August 2021',
-            end: 'May 2024',
-            location: 'College Park, MD',
-            points: [
-                "Earned my Bachelor's of Science (A.S.) in Computer Science",
-                "Part of the Dean's List: Fall 2022, Spring 2023, Fall 2023, Spring 2024",
-            ],
-            categories: ['Academic', 'University'],
-            link: 'https://umd.edu/',
-        },
-        {
-            title: "Pursuing my Associate's",
-            place: 'Edmonds College',
-            start: 'September 2019',
-            end: 'June 2021',
-            location: 'Lynnwood, WA',
-            points: [
-                "Earned my Associate's of Science (B.S.) in Computer Science",
-                'Earned a High School Diploma while taking college courses',
-            ],
-            categories: ['Academic', 'Community College'],
-            link: 'https://www.edmonds.edu/',
-        },
-    ];
+    const { t } = useTranslation();
 
     return (
-        <SectionContainer id='experiences'>
-            <Subheader text="Experiences" />
+        <SectionContainer id="experiences">
+            <Subheader text={t('STR_EXPERIENCE.TITLE')} />
 
             {/* Experiences List */}
             <ul className="group/menu flex flex-col gap-y-12">
-                {items.map((item, i) => (
-                    <li
-                        key={i}
-                        className={
-                            'group/item relative transition-all duration-300 ' +
-                            'group-hover/menu:opacity-50 hover:!opacity-100'
-                        }
-                    >
-                        {/* Hover Effect */}
-                        <div
+                {experiences.map((item, i) => {
+                    const [
+                        slug,
+                        title,
+                        place,
+                        start,
+                        end,
+                        location,
+                        points,
+                        categories,
+                        link,
+                    ] = [
+                        item.slug,
+                        t(item.titleKey),
+                        t(item.placeKey),
+                        t(item.startKey),
+                        t(item.endKey),
+                        t(item.locationKey),
+                        item.pointKeys.map((pointKey) => t(pointKey)),
+                        item.categories,
+                        item.link,
+                    ];
+
+                    return (
+                        <li
+                            id={`experience-item-${slug}`}
+                            key={i}
                             className={
-                                'bg-transparent absolute lg:-inset-y-4 lg:-inset-x-6 z-0 transition-colors rounded-lg  ' +
-                                'group-hover/item:shadow-sm group-hover/item:backdrop-blur-sm group-hover/item:bg-white/40 dark:group-hover/item:bg-gray-800/40'
+                                'group/item relative transition-all duration-300 ' +
+                                'group-hover/menu:opacity-50 hover:!opacity-100'
                             }
-                        ></div>
-
-                        {/* Overlay for link */}
-                        <a
-                            className="bg-transparent absolute inset-0 z-10 cursor-pointer"
-                            href={item.link}
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            aria-label={item.place + ' (opens in a new tab)'}
-                        />
-
-                        {/* Content */}
-                        <div className="z-5 relative flex flex-col gap-3">
-                            {/* Start - End | location */}
-                            <div className="uppercase text-xs flex items-center gap-2 text-surface-half-light dark:text-surface-half-dark">
-                                <span>{item.start}</span>
-                                <span>—</span>
-                                <span>
-                                    {item.end === '' ? 'present' : item.end}
-                                </span>
-                                <span>|</span>
-                                <span>{item.location}</span>
-                            </div>
-
-                            {/* Title - Place */}
+                        >
+                            {/* Hover Effect */}
                             <div
                                 className={
-                                    'text-surface-light dark:text-surface-dark ' +
-                                    'group-hover/item:text-accent-light dark:group-hover/item:text-accent-dark ' +
-                                    'text-lg font-medium flex flex-wrap gap-x-2 items-center'
+                                    'bg-transparent absolute lg:-inset-y-4 lg:-inset-x-6 z-0 transition-colors rounded-lg  ' +
+                                    'group-hover/item:shadow-sm group-hover/item:backdrop-blur-sm group-hover/item:bg-white/40 dark:group-hover/item:bg-gray-800/40'
                                 }
-                            >
-                                <span>{item.title}</span>
-                                <span>—</span>
-                                <span>
-                                    {item.place} {/* TR Arrow */}
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.7}
-                                        stroke="currentColor"
+                            ></div>
+
+                            {/* Overlay for link */}
+                            <a
+                                id={`experience-item-${slug}-link`}
+                                className="bg-transparent absolute inset-0 z-10 cursor-pointer"
+                                href={link}
+                                target="_blank"
+                                rel="noreferrer noopener"
+                                aria-label={t(
+                                    'STR_MISC.NAVIGATION.ARIA.OPENS_NEW_TAB',
+                                    {
+                                        target: place,
+                                    },
+                                )}
+                                title={place}
+                            />
+
+                            {/* Content */}
+                            <div className="z-5 relative flex flex-col gap-3">
+                                {/* Start - End | location */}
+                                <div className="uppercase text-xs flex items-center gap-2 text-surface-half-light dark:text-surface-half-dark">
+                                    <span>{start}</span>
+                                    <span>—</span>
+                                    <span
                                         className={
-                                            'size-4 inline transition-all transform ' +
-                                            'group-hover/item:translate-x-0.5 group-hover/item:-translate-y-0.5 ' +
-                                            'group-hover/item:text-accent-light/80 group-hover/item:dark:text-accent-dark/80'
+                                            'rounded-md px-2 py-0.5 ' +
+                                            (end === '' &&
+                                                'bg-accent-half-light/20 dark:bg-accent-half-dark/30 text-accent-light dark:text-accent-dark')
                                         }
                                     >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                                        />
-                                    </svg>
-                                </span>
-                            </div>
+                                        {end || t('STR_EXPERIENCE.PRESENT')}
+                                    </span>
+                                    <span>|</span>
+                                    <span>{location}</span>
+                                </div>
 
-                            {/* Bullet List of Points */}
-                            <ul className="list-disc ps-5 lg:ps-8 pe-5 flex flex-col gap-2 text-surface-half-light dark:text-surface-half-dark">
-                                {item.points.map((point, i) => (
-                                    <li key={i}>{point}</li>
-                                ))}
-                            </ul>
+                                {/* Title - Place */}
+                                <div
+                                    className={
+                                        'text-surface-light dark:text-surface-dark ' +
+                                        'group-hover/item:text-accent-light dark:group-hover/item:text-accent-dark ' +
+                                        'text-lg font-medium flex flex-wrap gap-x-2 items-center'
+                                    }
+                                >
+                                    <span>{title}</span>
+                                    <span>—</span>
+                                    <span>
+                                        {place} {/* TR Arrow */}
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth={1.7}
+                                            stroke="currentColor"
+                                            className={
+                                                'size-4 inline transition-all transform ' +
+                                                'group-hover/item:translate-x-0.5 group-hover/item:-translate-y-0.5 ' +
+                                                'group-hover/item:text-accent-light/80 group-hover/item:dark:text-accent-dark/80'
+                                            }
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                                            />
+                                        </svg>
+                                    </span>
+                                </div>
 
-                            {/* Category chips */}
-                            <div className="flex flex-wrap gap-2 mt-1">
-                                {item.categories.map((category, i) => (
-                                    <Chip key={i} text={category} />
-                                ))}
+                                {/* Bullet List of Points */}
+                                <ul className="list-disc ps-5 lg:ps-8 pe-5 flex flex-col gap-2 text-surface-half-light dark:text-surface-half-dark">
+                                    {points.map((point, i) => (
+                                        <li key={i}>{t(point)}</li>
+                                    ))}
+                                </ul>
+
+                                {/* Category chips */}
+                                <div className="flex flex-wrap gap-2 mt-1">
+                                    {categories.map((category, i) => (
+                                        <Chip key={i} text={category} />
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                    </li>
-                ))}
+                        </li>
+                    );
+                })}
             </ul>
         </SectionContainer>
     );
--- src/app/lib/utils/Constants.ts
+++ src/app/lib/utils/Constants.ts
@@ -1,5 +1,18 @@
+import { useTranslation } from 'react-i18next';
+
 export type Social = 'github' | 'linkedin' | 'spotify';
 export type Lang = { name: string; iconFile: string };
+export type Experience = {
+    slug: string;
+    titleKey: string;
+    placeKey: string;
+    startKey: string;
+    endKey: string;
+    locationKey: string;
+    pointKeys: string[];
+    categories: string[];
+    link: string;
+};
 
 /** A map of all social media links and pertinent information */
 export const socials: Record<
@@ -48,3 +61,86 @@ export const stack: Record<'core' | 'exp', Lang[]> = {
         { name: 'Next.js', iconFile: 'nextjs' },
     ],
 };
+
+const [inovalon, iigf, terrapinTech, bachelors, associates] = [
+    'INOVALON',
+    'IIGF',
+    'TERRAPIN_TECH',
+    'BACHELORS',
+    'ASSOCIATES',
+].map((e) => `STR_EXPERIENCE.ITEM.${e}`);
+
+/** A list of experiences along with the keys used for translations for some properties */
+export const experiences: Experience[] = [
+    {
+        slug: 'inovalon',
+        titleKey: `${inovalon}.TITLE`,
+        placeKey: `${inovalon}.PLACE`,
+        startKey: `${inovalon}.START`,
+        endKey: `${inovalon}.END`,
+        locationKey: `${inovalon}.LOCATION`,
+        pointKeys: [
+            `${inovalon}.POINT_1`,
+            `${inovalon}.POINT_2`,
+            `${inovalon}.POINT_3`,
+        ],
+        categories: [
+            'Angular',
+            'TypeScript',
+            'HTML + SCSS',
+            'PrimeNG',
+            'Python',
+        ],
+        link: 'https://www.inovalon.com/',
+    },
+    {
+        slug: 'iigf',
+        titleKey: `${iigf}.TITLE`,
+        placeKey: `${iigf}.PLACE`,
+        startKey: `${iigf}.START`,
+        endKey: `${iigf}.END`,
+        locationKey: `${iigf}.LOCATION`,
+        pointKeys: [`${iigf}.POINT_1`, `${iigf}.POINT_2`],
+        categories: [
+            'Next.js',
+            'React',
+            'Tailwind',
+            'JavaScript',
+            'HTML + CSS',
+        ],
+        link: 'https://www.ptpii.co.id/',
+    },
+    {
+        slug: 'terrapin-tech',
+        titleKey: `${terrapinTech}.TITLE`,
+        placeKey: `${terrapinTech}.PLACE`,
+        startKey: `${terrapinTech}.START`,
+        endKey: `${terrapinTech}.END`,
+        locationKey: `${terrapinTech}.LOCATION`,
+        pointKeys: [`${terrapinTech}.POINT_1`, `${terrapinTech}.POINT_2`],
+        categories: ['Technical Support', 'Communication', 'Leadership'],
+        link: 'https://it.umd.edu/',
+    },
+    {
+        slug: 'bachelors',
+        titleKey: `${bachelors}.TITLE`,
+        placeKey: `${bachelors}.PLACE`,
+        startKey: `${bachelors}.START`,
+        endKey: `${bachelors}.END`,
+        locationKey: `${bachelors}.LOCATION`,
+        pointKeys: [`${bachelors}.POINT_1`, `${bachelors}.POINT_2`],
+        categories: ['Academic', 'University'],
+        link: 'https://umd.edu/',
+    },
+    {
+        slug: 'associates',
+        titleKey: `${associates}.TITLE`,
+        placeKey: `${associates}.PLACE`,
+        startKey: `${associates}.START`,
+        endKey: `${associates}.END`,
+        locationKey: `${associates}.LOCATION`,
+        pointKeys: [`${associates}.POINT_1`, `${associates}.POINT_2`],
+        categories: ['Academic', 'Community College'],
+        link: 'https://www.edmonds.edu/',
+    },
+];
